8df3e663086aee62590be115fdcc5c9ef5cb1d57
Johnny <005330373@coyote.csusb.edu>
2023-09-25 16:58:00 -0700

    feat & fix: linting to scripts

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,9 @@ import { cert, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { readFile } from "fs/promises";
 
-const credential = JSON.parse(await readFile("./src/config/firebaseCredentials.json"));
+const credential = JSON.parse(
+  await readFile("./src/config/firebaseCredentials.json")
+);
 
 const firebase = initializeApp(
   {
