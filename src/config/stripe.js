df69bedc349cb57245aa198c81acd6be1cee06f5
thavisaj <005330373@coyote.csusb.edu>
2023-10-17 00:16:12 -0700

    feat & build: pin stripe api version and bump stripe

--- src/config/stripe.js
+++ src/config/stripe.js
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 import { STRIPE_SECRET_KEY } from "../utils/environment.js";
 
-const stripe = new Stripe(STRIPE_SECRET_KEY);
+const stripe = new Stripe(STRIPE_SECRET_KEY, {
+  apiVersion: "2023-08-16",
+});
 
 export default stripe;
