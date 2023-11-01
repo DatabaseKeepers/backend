66b374d87a941e4c6748f44f0059f7308ed33a50
thavisaj <005330373@coyote.csusb.edu>
2023-10-31 20:02:51 -0700

    fix: me api resulting in undefined cases

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -83,19 +83,22 @@ export async function images(req, res) {
 }
 
 export async function me(req, res) {
-  const result = await dbConn
-    .execute("SELECT role FROM User WHERE uid = ?", [req.userUID])
-    .catch((error) => {
-      console.log("user.service.me: ", error);
-    });
+  try {
+    const result = await dbConn.execute("SELECT role FROM User WHERE uid = ?", [
+      req.userUID,
+    ]);
 
-  if (result.size === 1) {
-    res.json({
-      role:
-        result.rows[0].role.charAt(0) +
-        result.rows[0].role.slice(1).toLowerCase(),
-    });
-  } else {
+    if (result.size === 1) {
+      res.json({
+        role:
+          result.rows[0].role.charAt(0) +
+          result.rows[0].role.slice(1).toLowerCase(),
+      });
+    } else {
+      res.json({ role: "Patient" });
+    }
+  } catch (error) {
+    console.log("user.service.me: ", error);
     res.json({ role: "Patient" });
   }
 }
