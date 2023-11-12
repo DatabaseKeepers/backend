2197d98f471ddee4a1f6c11c7b5128579fe898e9
thavisaj <005330373@coyote.csusb.edu>
2023-11-12 03:34:00 -0800

    feat: mark notifications as read api

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -3,3 +3,7 @@ import { notificationService } from "../services/index.js";
 export async function polling(req, res) {
   await notificationService.polling(req, res);
 }
+
+export async function read(req, res) {
+  await notificationService.read(req, res);
+}
