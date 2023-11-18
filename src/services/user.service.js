c1ba30f030231ea940e239809e72fadd43972589
thavisaj <005330373@coyote.csusb.edu>
2023-11-17 17:31:22 -0800

    feat: bump dependencies and remove uuid as one

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,4 @@
 import crypto from "crypto";
-import { v4 as uuidv4 } from "uuid";
 import dbConn from "../config/db.js";
 import {
   adminAuth,
@@ -40,7 +39,7 @@ export async function rateRadiologist(req, res) {
   const now = new Date();
 
   try {
-    const rating_uid = uuidv4();
+    const rating_uid = crypto.randomUUID();
     await dbConn
       .execute(
         "INSERT INTO \
